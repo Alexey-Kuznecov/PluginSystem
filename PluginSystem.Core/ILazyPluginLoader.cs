65744a5a8cfc1fb6bc3c4d603845a36ff6f91fe5
lexa1th@gmail.com <user03@DESKTOP-P3B43A9>
2025-04-14 10:41:13 +0800

    Добавлены ILazyPluginLoader и PluginStub; в дальнейшем планируется реализовать механизм ленивой загрузки плагинов для оптимизации ресурсов и повышения скорости запуска приложения, особенно при большом количестве доступных расширений.

---
+++ PluginSystem.Core/ILazyPluginLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginSystem.Core
+{
+    namespace PluginSystem.Core
+    {
+        /// <summary>
+        /// Представляет загрузчик плагинов с поддержкой ленивой (отложенной) инициализации.
+        /// Позволяет получать метаданные плагинов без полной загрузки и активировать их при необходимости.
+        /// </summary>
+        public interface ILazyPluginLoader
+        {
+            /// <summary>
+            /// Получает список "заглушек" плагинов, содержащих только метаданные и ленивую ссылку на сам плагин.
+            /// </summary>
+            IEnumerable<PluginStub> LoadAllPluginStubs();
+
+            /// <summary>
+            /// Получает метаданные плагина без его загрузки.
+            /// </summary>
+            PluginMetadata GetMetadata(string pluginPath);
+
+            /// <summary>
+            /// Загружает плагин по имени, но откладывает инициализацию до момента обращения.
+            /// </summary>
+            Lazy<IPlugin> LoadPluginLazy(string name);
+        }
+    }
+}
---
+++ PluginSystem.Core/PluginStub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginSystem.Core
+{
+    namespace PluginSystem.Core
+    {
+        /// <summary>
+        /// Заглушка плагина, содержащая метаданные и отложенную ссылку на загрузку плагина.
+        /// </summary>
+        public class PluginStub
+        {
+            /// <summary>
+            /// Метаданные плагина (имя, автор, версия и т.п.)
+            /// </summary>
+            public PluginMetadata Metadata { get; }
+
+            /// <summary>
+            /// Отложенная загрузка экземпляра плагина.
+            /// </summary>
+            public Lazy<IPlugin> Plugin { get; }
+
+            public PluginStub(PluginMetadata metadata, Func<IPlugin> pluginFactory)
+            {
+                Metadata = metadata;
+                Plugin = new Lazy<IPlugin>(pluginFactory);
+            }
+        }
+    }
+}
